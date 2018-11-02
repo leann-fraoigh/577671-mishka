1e9a2bad3f544f52752ab6dd8e3261997fb6acd2
Leann Fraoigh <d.myshlennikova@gmail.com>
2018-11-02 06:13:02 +0300

    Минифицированы js и html, улучшена загрузка шрифтов и доступность форм

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,17 +1,17 @@
-var pageHeader = document.querySelector('.page-header');
-var menuToggle = document.querySelector('.page-header__menu-toggle');
+var pageHeader = document.querySelector(".page-header");
+var menuToggle = document.querySelector(".page-header__menu-toggle");
 
-pageHeader.classList.remove('page-header--no-js');
+pageHeader.classList.remove("page-header--no-js");
 
 pageHeader.classList.add
-('page-header--menu-closed');
+("page-header--menu-closed");
 
-menuToggle.addEventListener('click', function() {
-  if (pageHeader.classList.contains('page-header--menu-closed')) {
-    pageHeader.classList.remove('page-header--menu-closed');
-    pageHeader.classList.add('page-header--menu-opened');
+menuToggle.addEventListener("click", function() {
+  if (pageHeader.classList.contains("page-header--menu-closed")) {
+    pageHeader.classList.remove("page-header--menu-closed");
+    pageHeader.classList.add("page-header--menu-opened");
   } else {
-    pageHeader.classList.add('page-header--menu-closed');
-    pageHeader.classList.remove('page-header--menu-opened');
+    pageHeader.classList.add("page-header--menu-closed");
+    pageHeader.classList.remove("page-header--menu-opened");
   }
 })
