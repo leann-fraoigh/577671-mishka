c2b32afb9af15172a98e2cf389b47af73885d39e
Leann Fraoigh <d.myshlennikova@gmail.com>
2018-11-08 19:13:15 +0300

    Улучшено единнобразие в js и переполнение в блоке статьи

--- source/js/menu.js
+++ source/js/menu.js
@@ -3,8 +3,7 @@ var menuToggle = document.querySelector(".page-header__menu-toggle");
 
 pageHeader.classList.remove("page-header--no-js");
 
-pageHeader.classList.add
-("page-header--menu-closed");
+pageHeader.classList.add("page-header--menu-closed");
 
 menuToggle.addEventListener("click", function() {
   if (pageHeader.classList.contains("page-header--menu-closed")) {
